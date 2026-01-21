d3e6dd883f1dbc68fe5c3188233dbd632e1dca7f
Ian Kirk <ikirk@Ians-MacBook-Air.local>
2026-01-21 13:25:23 -0600

    Created file structure and connected express server

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express(); 
+
+const port = 8080;
+app.get('/', (req, res) => {
+    res.json({
+        "Hello": "There"
+    })
+})
+app.listen(port, () => {
+    console.log(`Now listening on port http://localhost:${port}`)
+})
